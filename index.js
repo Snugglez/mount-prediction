30329aab66363b9052da7ac7326900647f2b6d7a
Snugglez <32309975+Snugglez@users.noreply.github.com>
2019-02-28 15:32:59 -0500

    optimization and debloating

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const	config = require('./config.json'),
 		path = require('path'),
 		fs = require('fs')
 module.exports = function mountpredict(d) {
-const 	mounts = require('./mountlist.json'),
-		grounds = require('./groundlist.json')
 
 let enabled = config.enabled,
 	onMount = null,
 	incontract = null,
-	skilldb = null
+	mountId = null,
+	mountSkill = null
 
 //commands o' plenty
 d.command.add("mp", {
@@ -20,15 +19,11 @@ off() {
 enabled = false
 d.command.message(`[disabled]`)
 },
-set() {
-enabled = false
-d.command.message(`[disabled]`)
-},
 unmount() {
 d.command.message(`[unmounted]`)
 d.send('S_UNMOUNT_VEHICLE', 2, {
 gameId: d.game.me.gameId,
-skill: skilldb
+skill: mountSkill
 })
 d.send('S_SHORTCUT_CHANGE', 2, {
 huntingZoneId: 7031,
@@ -50,30 +45,20 @@ d.command.message("--Dismounts you incase something bugs out and you can't")
 })
 
 //cStartSkill hook instant mount function
-d.hook('C_START_SKILL', (d.majorPatchVersion >= 74) ? 7 : 7, (e) => {
-if (!enabled || d.game.me.inCombat || incontract || !mounts[e.skill.id]) return
-skilldb = e.skill.id
+d.hook('C_START_SKILL', 7, (e) => {
+if(!enabled || d.game.me.inCombat || incontract) return
+if(e.skill.id == mountSkill && !onMount) {
 d.send('S_MOUNT_VEHICLE', 2, {
 gameId: d.game.me.gameId,
-id: mounts[skilldb],
-skill: skilldb,
+id: mountId,
+skill: mountSkill,
 unk: false
 })
-if (grounds.includes(mounts[skilldb])) {
-d.hookOnce('S_SHORTCUT_CHANGE', 2, (e) => {
-return false
-})
 }
-})
-
-//cStartSkill hook instant unmount function
-d.hook('C_START_SKILL', (d.majorPatchVersion >= 74) ? 7 : 7, (e) => {
-if(!enabled || !onMount || incontract) return;
-else if(mounts[e.skill.id] || e.skill.id === 65000001){
-
+else if(e.skill.id == mountSkill && onMount || e.skill.id == 65000001) {
 d.send('S_UNMOUNT_VEHICLE', 2, {
 gameId: d.game.me.gameId,
-skill: skilldb
+skill: mountSkill
 })
 d.send('S_SHORTCUT_CHANGE', 2, {
 huntingZoneId: 7031,
@@ -81,17 +66,12 @@ id: 300001,
 enable: false
 })
 }
-})
-
-//instant dive
-d.hook('C_START_SKILL', (d.majorPatchVersion >= 74) ? 7 : 7, (e) => {
-if(!enabled) return
-else if(e.skill.id === 65000002){
-d.send('S_START_CLIENT_CUSTOM_SKILL', (d.majorPatchVersion >= 74) ? 4 : 4, {
+else if(e.skill.id == 65000002){
+d.send('S_START_CLIENT_CUSTOM_SKILL', 4, {
 gameId: d.game.me.gameId,
 skill: 65000002
 })
-d.hookOnce('S_START_CLIENT_CUSTOM_SKILL', (d.majorPatchVersion >= 74) ? 4 : 4, (e) => {return false})
+d.hookOnce('S_START_CLIENT_CUSTOM_SKILL', 4, (e) => {return false})
 }
 })
 
@@ -110,8 +90,11 @@ enable: false
 //shit fix if the client is stuck incontract
 d.hook('S_MOUNT_VEHICLE', 2, (e) => {
 if(!enabled) return;
-if(d.game.me.is(e.gameId))
+if(d.game.me.is(e.gameId)){
+mountId = e.id
+mountSkill = e.skill
 incontract = false
+}
 })
 
 //sSystemMessage to instantly unmount in unmountable zones
@@ -131,6 +114,5 @@ skill: 12200016
 	d.hook('S_REJECT_CONTRACT', 1, e => { incontract = false })
 	d.hook('S_CANCEL_CONTRACT', 1, e => { incontract = false })
 	d.hook('S_GACHA_END', 1, e => { incontract = false })
-	d.hook('S_LOAD_TOPO', 3, e => { incontract = false })
 	d.hook('C_BIND_ITEM_EXECUTE', 1, e => { incontract = false })
 }
